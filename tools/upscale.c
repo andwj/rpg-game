f3566a21b88110f86b219faf59b4b0e689776da7
Andrew Apted <ajapted@users.sf.net>
2015-03-30 17:08:48 +1100

    Upscale : code to read the input image and write the output image.

--- tools/upscale.c
+++ tools/upscale.c
@@ -8,12 +8,66 @@
 #include <stdlib.h>
 
 
-int trans_color[3] = { 0x21, 0x22, 0x23 };
+const int input_trans = 0x313233;
+
+#define WIDTH	(10 * 32)
+#define HEIGHT	(26 * 32)
+
+int in_pix[WIDTH][HEIGHT];
+
+int out_pix[WIDTH*2][HEIGHT*2];
+
+
+int read_pixel()
+{
+	int r = getchar();
+	int g = getchar();
+	int b = getchar();
+
+	return (r << 16) | (g << 8) | b;
+}
+
+
+void write_pixel(int pix)
+{
+	int r = (pix & 0xff0000) >> 16;
+	int g = (pix & 0x00ff00) >>  8;
+	int b = (pix & 0x0000ff);
+
+	putchar(r);
+	putchar(g);
+	putchar(b);
+}
 
 
 int main()
 {
-	// TODO
+	int i;
+	int x, y;
+
+	// skip header
+	for (i = 0 ; i < 15 ; i++)
+	{
+		getchar();
+	}
+
+	// read pixels
+	for (y = 0 ; y < HEIGHT ; y++)
+	for (x = 0 ; x < WIDTH  ; x++)
+	{
+		in_pix[x][y] = read_pixel();	
+	}
+
+	// FIXME : process
+
+	// write PPM file
+	printf("P6\n%d %d\n255\n", WIDTH*2, HEIGHT*2);
+	
+	for (y = 0 ; y < HEIGHT*2 ; y++)
+	for (x = 0 ; x < WIDTH*2  ; x++)
+	{
+		write_pixel(out_pix[x][y]);
+	}
 }
 
 // --- editor settings ----
