ac9c8caed20778783a498a904f473a271bde24fe
Andrew Apted <ajapted@users.sf.net>
2015-03-30 16:51:45 +1100

    Began work on a tool to upscale the tileset image (using 2xSaI algorithm).

---
+++ tools/upscale.c
@@ -0,0 +1,20 @@
+/*
+ * Upscale the tileset.ppm image using the 2xSaI algorithm.
+ * 
+ * 2xSaI code is by Derek Liauw ("Kreed") and under the GNU GPL v2+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int trans_color[3] = { 0x21, 0x22, 0x23 };
+
+
+int main()
+{
+	// TODO
+}
+
+// --- editor settings ----
+// vi:ts=4:sw=4:noexpandtab
