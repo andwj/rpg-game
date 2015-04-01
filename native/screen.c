1a53a64d7b44e62692c6bfad455572feeb21fe5c
Andrew Apted <ajapted@users.sf.net>
2015-04-01 19:33:45 +1100

    Native : init the allegro font and TTF addons.

--- native/screen.c
+++ native/screen.c
@@ -39,6 +39,8 @@ void Screen_Init(void)
 	al_init_native_dialog_addon();
 	al_init_primitives_addon();
 	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
 
 	al_install_mouse();
 	al_install_keyboard();
