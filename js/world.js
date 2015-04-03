00cd0894a56ab980d098830e0a5662e68e8cd838
Andrew Apted <ajapted@users.sf.net>
2015-04-04 00:18:39 +1100

    World : began work on a 'Tile' class, and implemented creating the 'World' global object and populating the array of tile refs (initially all null).

--- js/world.js
+++ js/world.js
@@ -9,8 +9,66 @@
 "use strict";
 
 
+// Tile class
+
+var Tile = function(kind)
+{
+	// the kind of tile [ FIXME : describe ]
+	this.kind = kind;
+
+	// all the objects here
+	this.objects = [];
+
+	// the monster or player here (if any)
+	this.actor = null;
+};
+
+
+Tile.prototype =
+{
+	sayHello: function()
+	{
+		render_AddText(this.info.kind + " says hello!\n");
+	}
+};
+
+
+//________________________________________________
+
+
+function world_NewTileRow(w)
+{
+	var row = [];
+
+	for (var i = 0 ; i < w ; i++)
+		row.push(null);
+
+	return row;
+}
+
+
 function world_NewGame()
 {
+	World =
+	{
+		tiles: [],
+
+		areas: [],
+
+		entities: []
+	};
+
+
+	// create tile grid
+	// Note: unused tiles are _null_!
+
+	World.tw = 60;
+	World.th = 60;
+
+	for (var ty = 0 ; ty < World.th ; ty++)
+	{
+		World.tiles.push(world_NewTileRow);
+	}
 }
 
 
