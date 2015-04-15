c9566831f327c6af542015eb6d096a9c5d3a74a5
Andrew Apted <ajapted@users.sf.net>
2015-04-15 23:26:56 +1000

    New code file 'lang.js' -- will contain language stuff, e.g. random player names, chat logic, and so forth.

---
+++ js/lang.js
@@ -0,0 +1,14 @@
+//
+//  LANGUAGE STUFF
+//
+//  Copyright (c) 2015 Andrew Apted
+//
+//  under the GNU GPL v3 license
+//______________________________________
+
+
+function lang_NamePlayer(class_, race)
+{
+  // TODO
+}
+
